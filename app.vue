6f7564fdd81cad96bd934a41cffa932bc07164ae
Maxime Coriton <maxime.coriton@gmail.com>
2022-12-09 17:01:03 +0100

    test

--- app.vue
+++ app.vue
@@ -1,11 +1,6 @@
 <script setup>
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
-
-/* import { Amplify } from 'aws-amplify'; */
-/*   import awsconfig from './aws-exports'; */
-
-/*   Amplify.configure(awsconfig); */
 </script>
 
 <template>
